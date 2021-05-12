51d0bfae4273470b814d6505a1437a5e9f92f5fd
Dan Xu <danxu@dans-mbp.lan>
2021-05-11 22:28:48 -0700

    recursion solution

--- 199. Binary Tree Right Side View.java
+++ 199. Binary Tree Right Side View.java
@@ -34,3 +34,44 @@ class Solution {
         return result;
     }
 }
+
+//recursion
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        result.add(root.val);
+        List<Integer> right = new ArrayList<>();
+        List<Integer> left = new ArrayList<>();
+        if (root.right != null) right = rightSideView(root.right);
+        if (root.left != null) left = rightSideView(root.left);
+
+        if (right == null) result.addAll(left);
+        if (left == null) result.addAll(right);
+
+        if (right != null && left != null) {
+            result.addAll(right);
+            if (right.size() < left.size())
+                result.addAll(left.subList(right.size(), left.size()));
+
+        }
+        return result;
+    }
+}
