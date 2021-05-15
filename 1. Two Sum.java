278f0e0edd47ca7f4e086638e5b5b35e1d441ce2
Dan Xu <danxu@dans-mbp.lan>
2021-05-15 10:38:51 -0700

    two-pointers solution added

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -10,3 +10,24 @@ class Solution {
         return new int[2];
     }
 }
+
+// two-pointers solution
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[][] newNums = new int[nums.length][];
+        for (int i = 0; i < nums.length; i++) {
+            newNums[i] = new int[]{nums[i], i};
+        }
+
+        Arrays.sort(newNums, (a, b) -> Integer.compare(a[0], b[0]));
+        int i = 0, j = nums.length - 1;
+        while(i < j) {
+            if (newNums[i][0] + newNums[j][0] == target) return new int[]{newNums[i][1], newNums[j][1]};
+            else if (newNums[i][0] + newNums[j][0] > target) j--;
+            else i++;
+        }
+        return new int[2];
+
+    }
+}
