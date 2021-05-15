c210b666f3e93010d5b1a496ddb6829f010e3a10
Dan Xu <danxu@dans-mbp.lan>
2021-05-15 12:07:53 -0700

    two pointers without set solution

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -47,3 +47,32 @@ class Solution {
         return new ArrayList(set);
     }
 }
+
+// two-pointers without set
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int target = -nums[i];
+                int j = i + 1, k = nums.length - 1;
+                while (j < k) {
+                    if (nums[j] +  nums[k] == target) {
+                        result.add(Arrays.asList(nums[j], nums[k], nums[i]));
+                        j++;
+                        k--;
+                        while(j < k && nums[j] == nums[j - 1]) j++;
+                        while(j < k && nums[k] == nums[k + 1]) k--;
+                    } else if (nums[j] + nums[k] < target) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
