5d549bf440686facd511a19c6cec698cc940c26e
Dan Xu <danxu@dans-mbp.lan>
2021-05-19 00:32:56 -0700

    divide and conquer solution

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -1,11 +1,36 @@
 class Solution {
+    int[] numsArray;
     public int maxSubArray(int[] nums) {
-        int maxSofar = nums[0], maxEnding = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            maxEnding = Math.max(maxEnding + nums[i], nums[i]);
-            maxSofar = Math.max(maxSofar, maxEnding);
+        numsArray = nums;
+        return findBestSubarray(0, numsArray.length - 1);
+    }
+
+    public int findBestSubarray(int left, int right) {
+        if (left > right) return Integer.MIN_VALUE;
+
+        int mid = Math.floorDiv(left + right, 2);
+        int curr = 0;
+        int bestLeftSum = 0;
+        int bestRightSum = 0;
+
+        for (int i = mid - 1; i >= left; i--) {
+            curr += numsArray[i];
+            bestLeftSum = Math.max(curr, bestLeftSum);
         }
-        return maxSofar;
+
+        curr = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            curr += numsArray[i];
+            bestRightSum = Math.max(curr, bestRightSum);
+        }
+
+        int bestMidSum = bestRightSum + numsArray[mid] + bestLeftSum;
+
+        int findBestLeftSum = findBestSubarray(left, mid - 1);
+        int findBestRightSum = findBestSubarray(mid + 1, right);
+
+        return Math.max(bestMidSum, Math.max(findBestLeftSum, findBestRightSum));
+    }
 
     }
 }
