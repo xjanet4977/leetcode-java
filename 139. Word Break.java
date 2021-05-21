c7a25109cd7764d9ab1c203e373095ea39e955dd
Dan Xu <danxu@dans-mbp.lan>
2021-05-21 12:54:50 -0700

    dp solution

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return wordBreak(s, new HashSet(wordDict), 0, new Boolean[s.length()]);
-    }
+        Set<String> dict = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
 
-    public boolean wordBreak(String s, Set<String> wordDict, int start, Boolean[] memo) {
-        if (start == s.length()) return true;
-        if (memo[start] != null) return memo[start];
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordDict.contains(s.substring(start, end)) && wordBreak(s, wordDict, end, memo)) {
-                memo[start] = true;
-                return true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        memo[start] = false;
-        return false;
-    }
 
+        return dp[s.length()];
+    }
 }
